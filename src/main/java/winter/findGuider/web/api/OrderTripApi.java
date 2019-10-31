73d695eb2d2f71f004bf34d0b0d73a43954805a6
HoangHai <haihse05793@fpt.edu.vn>
2019-11-01 01:25:43 +0900

    commit get closest finish date service

--- src/main/java/winter/findGuider/web/api/OrderTripApi.java
+++ src/main/java/winter/findGuider/web/api/OrderTripApi.java
@@ -29,4 +29,16 @@ public class OrderTripApi {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
+
+    @RequestMapping("/GetClosestFinishDate")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> getClosestFinishDate(@RequestBody Order newOrder) {
+        try {
+            String finishDate = orderTripService.getClosestTourFinishDate(newOrder.getBegin_date().toLocalDate(),
+                    newOrder.getGuider_id());
+            return new ResponseEntity<>(finishDate, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
 }
