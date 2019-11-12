04100d05d6b187c557ac8ed2a9f34fa2c0a56853
HoangHai <haihse05793@fpt.edu.vn>
2019-11-13 01:40:58 +0900

    update review

--- src/main/java/services/Review/ReviewServiceImpl.java
+++ src/main/java/services/Review/ReviewServiceImpl.java
@@ -25,6 +25,23 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
+    public List<Review> findReviewByOrderId(long order_id) {
+        try {
+            String query = "select * from review where order_id = ?";
+            return jdbcTemplate.query(query, new RowMapper<Review>() {
+                public Review mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    return new Review(rs.getLong("order_id"), rs.getLong("traveler_id"),
+                            rs.getLong("guider_id"), rs.getLong("post_id"), rs.getLong("rated"),
+                            rs.getDate("post_date"), rs.getString("review"));
+                }
+            }, order_id);
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
     public List<Review> findReviewsByGuiderId(long guider_id) {
         try {
             String query = "select * from review where guider_id = ?";
@@ -73,21 +90,16 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public boolean checkReviewable(long post_id, long traveler_id) {
+    public boolean checkReviewExist(long order_id) {
         try {
-            String query = "select order_id from ordertrip where traveler_id = ? " +
-                    "and status = 'ONGOING' or status = 'FINISHED' " +
-                    "except select order_id from review";
+            String query = "select count(order_id) from review where order_id = ?";
             List<Integer> checklist = jdbcTemplate.query(query, new RowMapper<Integer>() {
                 @Override
                 public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    return rs.getInt("order_id");
+                    return rs.getInt("count");
                 }
-            }, traveler_id);
-            if (checklist == null || checklist.isEmpty()) {
-                return false;
-            }
-            if (checklist.contains(post_id)) {
+            }, order_id);
+            if (checklist.get(0) == 0) {
                 return true;
             } else {
                 return false;
