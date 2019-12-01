226b1494dd071a18719f3036ff96adf83872c6bc
HoangHai <haihse05793@fpt.edu.vn>
2019-12-02 01:11:29 +0900

    Update TripService.java

--- src/main/java/services/trip/TripService.java
+++ src/main/java/services/trip/TripService.java
@@ -9,32 +9,31 @@ import java.util.Date;
 import java.util.List;
 
 public interface TripService {
-    public void createTrip(Order newOrder) throws Exception;
+    void createTrip(Order newOrder) throws Exception;
 
-    public int checkTripExist(int id) throws Exception;
+    int checkTripExist(int id) throws Exception;
 
-    public Order findTripById(int trip_id) throws Exception;
+    Order findTripById(int trip_id) throws Exception;
 
-    public List<Order> findTripByStatus(String role, int id, String status) throws Exception;
+    List<Order> findTripByStatus(String role, int id, String status) throws Exception;
 
-    public boolean acceptTrip(int trip_id) throws Exception;
+    boolean acceptTrip(int trip_id) throws Exception;
 
-    public boolean cancelTrip(int trip_id) throws Exception;
+    boolean cancelTrip(int trip_id) throws Exception;
 
-    public boolean finishTrip(int trip_id) throws Exception;
+    boolean finishTrip(int trip_id) throws Exception;
 
-    public void getTripGuiderId_FinishDate(Order newOrder) throws Exception;
+    void getTripGuiderId_FinishDate(Order newOrder) throws Exception;
 
-    public int checkAvailabilityOfTrip(Order newOrder) throws Exception;
+    int checkAvailabilityOfTrip(Order newOrder) throws Exception;
 
-    public ArrayList<String> getGuiderAvailableHours(LocalDate date, int post_id, int guider_id) throws Exception;
+    ArrayList<String> getGuiderAvailableHours(LocalDate date, int post_id, int guider_id) throws Exception;
 
-    public String getClosestTripFinishDate(LocalDate date, int guider_id) throws Exception;
+    String getClosestTripFinishDate(LocalDate date, int guider_id) throws Exception;
 
-    public List<Order> getTripByWeek(int id, Date start, Date end) throws Exception;
+    List<Order> getTripByWeek(int id, Date start, Date end) throws Exception;
 
-    public boolean checkTripReach48Hours(Order cancelOrder, LocalDateTime rightNow) throws Exception;
+    boolean checkTripReach48Hours(Order cancelOrder, LocalDateTime rightNow) throws Exception;
 
-    public String getExpectedEndTripTime(int post_id, LocalDateTime begin_date) throws Exception;
-
+    String getExpectedEndTripTime(int post_id, LocalDateTime begin_date) throws Exception;
 }
