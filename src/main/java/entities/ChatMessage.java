3697a02f63be4501f7c24c49fb6c259d00bece51
Nguyen Duc Dung <dungnguyen@Nguyens-MacBook-Pro-2.local>
2019-12-14 01:05:24 +0700

    update Chat message entities

--- src/main/java/entities/ChatMessage.java
+++ src/main/java/entities/ChatMessage.java
@@ -13,27 +13,12 @@ public class ChatMessage {
     @Id
     private String id;
     private String content;
-    private String user;
-    private String receiver;
+    private String guider;
+    private String traveler;
     private Date dateReceived;
     private boolean isSeen;
     private String type;
-
-    public boolean isSeen() {
-        return isSeen;
-    }
-
-    public void setSeen(boolean seen) {
-        isSeen = seen;
-    }
-
-    public Date getDateReceived() {
-        return dateReceived;
-    }
-
-    public void setDateReceived(Date dateReceived) {
-        this.dateReceived = dateReceived;
-    }
+    private String sender;
 
     public String getId() {
         return id;
@@ -41,14 +26,6 @@ public class ChatMessage {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getReceiver() {
-        return receiver;
-    }
-
-    public void setReceiver(String receiver) {
-        this.receiver = receiver;
     }
 
     public String getContent() {
@@ -59,12 +36,36 @@ public class ChatMessage {
         this.content = content;
     }
 
-    public String getUser() {
-        return user;
+    public String getGuider() {
+        return guider;
     }
 
-    public void setUser(String sender) {
-        this.user = sender;
+    public void setGuider(String guider) {
+        this.guider = guider;
+    }
+
+    public String getTraveler() {
+        return traveler;
+    }
+
+    public void setTraveler(String traveler) {
+        this.traveler = traveler;
+    }
+
+    public Date getDateReceived() {
+        return dateReceived;
+    }
+
+    public void setDateReceived(Date dateReceived) {
+        this.dateReceived = dateReceived;
+    }
+
+    public boolean isSeen() {
+        return isSeen;
+    }
+
+    public void setSeen(boolean seen) {
+        isSeen = seen;
     }
 
     public String getType() {
@@ -73,5 +74,13 @@ public class ChatMessage {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
     }
 }
